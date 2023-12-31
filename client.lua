81c306100fb82375b7026981387df107a10cbe0e
Doğacan Koç <74499668+Doxria@users.noreply.github.com>
2023-12-31 08:21:14 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -16,7 +16,7 @@ CreateThread(function()
 	end
 	QBCore.Functions.TriggerCallback('ik-moneywash:server:pedcoords', function(pedloc)
 		RequestModel(Config.pedmodel) while not HasModelLoaded(Config.pedmodel) do Wait(1) end
-		pedman = CreatePed(0, Config.pedmodel , pedloc.x, pedloc.y, pedloc.z, pedloc.w, false, false)
+		pedman = CreatePed(0, Config.pedmodel, pedloc.x, pedloc.y, pedloc.z, pedloc.w, false, false)
 		SetEntityInvincible(pedman, true)
 		bagspawned = CreateObject(GetHashKey(bagModel), pedloc.x +2, pedloc.y+2, pedloc.z, 1, 1, 1)
 		Wait(1000)
@@ -29,37 +29,36 @@ CreateThread(function()
         TaskPlayAnim(pedman, baganimDict, baganimName, 1.0, -1, -1, 50, 0, 0, 0, 0)
 		SetBlockingOfNonTemporaryEvents(pedman, true)
 		FreezeEntityPosition(pedman, true)
-		SetEntityNoCollisionEntity(pedman, PlayerPedId(), false)
 
 		if Config.OpenWithItem then
-			exports['qb-target']:AddCircleZone("WashingPed", vector3(pedloc.x, pedloc.y, pedloc.z), 2.0, { name="WashingPed", debugPoly=false, useZ=true, },{ options = { { event = "ik-moneywash:client:ShowMenu", icon = "fas fa-certificate", label = "Open Menu", item = Config.ItemName}, }, distance = 2.0 })
+			exports['qb-target']:AddCircleZone("WashingPed", vector3(pedloc.x, pedloc.y, pedloc.z), 2.0, { name="WashingPed", debugPoly=false, useZ=true, },{ options = { { event = "ik-moneywash:client:ShowMenu", icon = "fas fa-certificate", label = Lang:t('target.open'), item = Config.ItemName}, }, distance = 2.0 })
 		else
-			exports['qb-target']:AddCircleZone("WashingPed", vector3(pedloc.x, pedloc.y, pedloc.z), 2.0, { name="WashingPed", debugPoly=false, useZ=true, },{ options = { { event = "ik-moneywash:client:ShowMenu", icon = "fas fa-certificate", label = "Open Menu"}, }, distance = 2.0 })
+			exports['qb-target']:AddCircleZone("WashingPed", vector3(pedloc.x, pedloc.y, pedloc.z), 2.0, { name="WashingPed", debugPoly=false, useZ=true, },{ options = { { event = "ik-moneywash:client:ShowMenu", icon = "fas fa-certificate", label = Lang:t('target.open')}, }, distance = 2.0 })
 		end
 	end)
 end)
 
 RegisterNetEvent("ik-moneywash:client:ShowMenu", function()
     local MoneyMenu = {}
-    MoneyMenu[#MoneyMenu+1] = { header = "Washing Menu", txt = "", isMenuHeader = true }
-    MoneyMenu[#MoneyMenu+1] = { icon = "fas fa-circle-xmark", header = "", txt = "Close", params = { event = "ik-moneywash:client:Menu:Close" } }
+    MoneyMenu[#MoneyMenu+1] = { header = Lang:t('menu.washing_header'), txt = "", isMenuHeader = true }
+    MoneyMenu[#MoneyMenu+1] = { icon = "fas fa-circle-xmark", header = "", txt = Lang:t('menu.close'), params = { event = "ik-moneywash:client:Menu:Close" } }
 
-    MoneyMenu[#MoneyMenu+1] = { header = "Wash Black Money", txt = "Wash black money and receive legal money" ,params = { event = "ik-moneywash:client:WashInput",}}
+    MoneyMenu[#MoneyMenu+1] = { header = Lang:t('menu.wash_money_header'), txt = Lang:t('menu.wash_money_text'), params = { event = "ik-moneywash:client:WashInput",}}
 
     exports['qb-menu']:openMenu(MoneyMenu)
 end)
 
 RegisterNetEvent("ik-moneywash:client:WashInput", function()
     local header = "<center><p><img src=nui://"..Config.img..QBCore.Shared.Items[Config.BlackMoneyName].image.." width=100px></p>"..QBCore.Shared.Items[Config.BlackMoneyName].label
-	local settext = "- How much ? -"
+	local settext = Lang:t('menu.how_much')
 	local NewInput = {}
 	NewInput[#NewInput+1] = { type = 'number', isRequired = true, name = 'amount', text = settext }
-	local dialog = exports['qb-input']:ShowInput({ header = header, submitText = "Change", inputs = NewInput })
+	local dialog = exports['qb-input']:ShowInput({ header = header, submitText = Lang:t('menu.change'), inputs = NewInput })
 	if dialog then
 		if tonumber(dialog.amount) >= 0 then
 			TriggerServerEvent("ik-moneywash:server:sellthem", dialog.amount)
 		else
-			QBCore.Functions.Notify("Value must be higher then 0", "error")
+			QBCore.Functions.Notify(Lang:t('error.no_value'), "error")
 		end
 	end
 end)
